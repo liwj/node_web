6d5a1c65d0df425bf9efa8baa79f242025a281b8
liwj <road_ok@163.com>
2014-03-10 18:37:31 +0800

    添加了数据库连接信息

---
+++ dao/db.js
@@ -0,0 +1,10 @@
+/**
+ * 数据库JS
+ */
+var setting = require('setting');
+//mongodb模块
+var Db = require('mongodb').Db;
+var conn = require('mongodb').Connection;
+var server = require('mongodb').Server;
+//创建数据库连接
+module.exports = new Db(setting.db,new Server(setting.host,conn.DEFAULE.PORT,{})); 
---
+++ dao/settings.js
@@ -0,0 +1,8 @@
+/**
+ * DB连接信息
+ */
+module.exports = {
+		db:'weibo',//数据库名
+		host:'127.0.0.1',//端口
+		cookieSecret:'weiboroad'//用于cookie加密
+};
